72f175c19ed6905584093e3266adb33e74a77ca5
Бельянинов Евгений Сергеевич <ext.ebelyaninov@tinkoff.ru>
2022-01-17 10:01:34 +0000

    Добавил методы и тесты:

---
+++ src/test/java/stpTrackingAdminApi/getSignals/GetSignalsTest.java
@@ -0,0 +1,364 @@
+package stpTrackingAdminApi.getSignals;
+
+import extenstions.RestAssuredExtension;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.junit5.AllureJunit5;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.qa.tinkoff.allure.Subfeature;
+import ru.qa.tinkoff.investTracking.configuration.InvestTrackingAutoConfiguration;
+import ru.qa.tinkoff.investTracking.entities.MasterSignal;
+import ru.qa.tinkoff.investTracking.services.MasterSignalDao;
+import ru.qa.tinkoff.kafka.configuration.KafkaAutoConfiguration;
+import ru.qa.tinkoff.social.configuration.SocialDataBaseAutoConfiguration;
+import ru.qa.tinkoff.social.services.database.ProfileService;
+import ru.qa.tinkoff.steps.StpTrackingAdminStepsConfiguration;
+import ru.qa.tinkoff.steps.trackingAdminSteps.StpTrackingAdminSteps;
+import ru.qa.tinkoff.swagger.investAccountPublic.model.GetBrokerAccountsResponse;
+import ru.qa.tinkoff.swagger.tracking_admin.api.SignalApi;
+import ru.qa.tinkoff.swagger.tracking_admin.invoker.ApiClient;
+import ru.qa.tinkoff.swagger.tracking_admin.model.GetSignalsResponse;
+import ru.qa.tinkoff.swagger.tracking_admin.model.Signal;
+import ru.qa.tinkoff.tracking.configuration.TrackingDatabaseAutoConfiguration;
+import ru.qa.tinkoff.tracking.entities.enums.*;
+import ru.qa.tinkoff.tracking.services.database.*;
+
+import java.math.BigDecimal;
+import java.time.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static io.qameta.allure.Allure.step;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Epic("getSignals - Получение списка сигналов на стратегии")
+@Feature("TAP-13486")
+@ExtendWith({AllureJunit5.class, RestAssuredExtension.class})
+@DisplayName("stp-tracking-admin")
+@Subfeature("Успешные сценарии")
+@Tags({@Tag("stp-tracking-admin"), @Tag("getSignals")})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = {
+    TrackingDatabaseAutoConfiguration.class,
+    SocialDataBaseAutoConfiguration.class,
+    KafkaAutoConfiguration.class,
+    StpTrackingAdminStepsConfiguration.class,
+    InvestTrackingAutoConfiguration.class
+})
+public class GetSignalsTest {
+
+    @Autowired
+    ClientService clientService;
+    @Autowired
+    ContractService contractService;
+    @Autowired
+    ProfileService profileService;
+    @Autowired
+    TrackingService trackingService;
+    @Autowired
+    StpTrackingAdminSteps stpTrackingAdminSteps;
+    @Autowired
+    StrategyService strategyService;
+    @Autowired
+    MasterSignalDao masterSignalDao;
+
+    String xApiKey = "x-api-key";
+    String key= "tracking";
+
+
+    String tickerSBER = "SBER";
+    String tradingClearingAccountSber = "L01+00002F00";
+    String tickerAAPL = "AAPL";
+    String tradingClearingAccountAAPL = "TKCBM_TCAB";
+    String tickerFB = "FB";
+    String tradingClearingAccountFB = "TKCBM_TCAB";
+    String tickerNok = "NOK";
+    String tradingClearingAccountNok = "L01+00000SPB";
+
+
+    SignalApi signalApi = ru.qa.tinkoff.swagger.tracking_admin.invoker.ApiClient.api(ApiClient.Config.apiConfig()).signal();
+
+    String siebelIdMaster = "5-DYNN1E3S";
+    String contractIdMaster;
+    UUID strategyId;
+    LocalDateTime localDateTime;
+    UUID investIdMaster;
+
+    @BeforeAll
+    void getDataFromAccount(){
+        //получаем данные по клиенту master в api сервиса счетов
+        GetBrokerAccountsResponse resAccountMaster = stpTrackingAdminSteps.getBrokerAccounts(siebelIdMaster);
+        investIdMaster = resAccountMaster.getInvestId();
+        contractIdMaster = resAccountMaster.getBrokerAccounts().get(0).getId();
+    }
+
+    @BeforeEach
+    void createClient() {
+        strategyId = UUID.randomUUID();
+        localDateTime = LocalDateTime.now();
+        int randomNumber = 0 + (int) (Math.random() * 1000);
+        String title = "Autotest" +String.valueOf(randomNumber);
+        String description = "new test стратегия autotest";
+        //создаем в БД tracking данные: client, contract, strategy в статусе active
+        stpTrackingAdminSteps.createClientWithContractAndStrategyNew(siebelIdMaster, investIdMaster, ClientRiskProfile.aggressive, contractIdMaster, null, ContractState.untracked,
+            strategyId, title, description, StrategyCurrency.usd, StrategyRiskProfile.aggressive,
+            StrategyStatus.active, 0, LocalDateTime.now().minusDays(1));
+    }
+
+    @AfterEach
+    void deleteClient() {
+        step("Удаляем клиента автоследования", () -> {
+            try {
+                masterSignalDao.deleteMasterSignalByStrategy(strategyId);
+            } catch (Exception e) {
+            }
+            try {
+                strategyService.deleteStrategy(stpTrackingAdminSteps.strategy);
+            } catch (Exception e) {
+            }
+            try {
+                contractService.deleteContract(stpTrackingAdminSteps.contract);
+            } catch (Exception e) {
+            }
+            try {
+                clientService.deleteClient(stpTrackingAdminSteps.client);
+            } catch (Exception e) {
+            }
+        });
+    }
+
+
+    @SneakyThrows
+    @Test
+    @AllureId("1458345")
+    @DisplayName("C1458345.GetSignals.Получение списка сигналов без указания cursor & limit")
+    @Subfeature("Успешные сценарии")
+    @Description("Получение списка сигналов на стратегии")
+    void C1458345() {
+
+        createMasterSignal(0, 3, 1, strategyId, tickerSBER, tradingClearingAccountSber,
+            "4289.37", "10", 11);
+        createMasterSignal(0, 2, 2, strategyId, tickerAAPL, tradingClearingAccountAAPL,
+            "128.37", "3", 12);
+        createMasterSignal(0, 1, 3, strategyId, tickerFB, tradingClearingAccountFB,
+            "500.37", "10", 13);
+
+        List<MasterSignal> getAllMasterSignals = masterSignalDao.getAllMasterSignal(strategyId);
+        GetSignalsResponse getdataFromResponce = getSignalsResponse(strategyId);
+       //Сортируем версию по убыванию
+        getAllMasterSignals.stream()
+            .sorted(Comparator.comparing(MasterSignal::getVersion).reversed())
+            .collect(Collectors.toList());
+
+        //Получаем items из ответа метода и БД, для инструментов
+        List<Signal> getItemsForSBER = getdataFromResponce.getItems().stream()
+            .filter(res -> res.getExchangePosition().getTicker().equals(tickerSBER))
+            .collect(Collectors.toList());
+
+        Optional<MasterSignal> getDataFromDaoForSBER =  getAllMasterSignals.stream()
+            .filter(signals -> signals.getTicker().equals(tickerSBER))
+            .findFirst();
+
+        List<Signal> getItemsForAAPL = getdataFromResponce.getItems().stream()
+            .filter(res -> res.getExchangePosition().getTicker().equals(tickerAAPL))
+            .collect(Collectors.toList());
+
+        Optional<MasterSignal> getDataFromDaoForAPPL =  getAllMasterSignals.stream()
+            .filter(signals -> signals.getTicker().equals(tickerAAPL))
+            .findFirst();
+
+        assertThat("nextCursor != 1", getdataFromResponce.getNextCursor(),
+            is(getAllMasterSignals.get(2).getVersion().toString()));
+        assertThat("hasNext != false", getdataFromResponce.getHasNext(), is(false));
+        //Проверяем выгрузку 2 items c action = 13 игнорируем
+        assertThat("получили больше 2 items в ответе метода", getdataFromResponce.getItems().size(),
+            is(2));
+
+        checkItemsFromResponce(getItemsForAAPL, getDataFromDaoForAPPL, "buy", StrategyCurrency.usd);
+        checkItemsFromResponce(getItemsForSBER, getDataFromDaoForSBER, "sell", StrategyCurrency.usd);
+    }
+
+    @SneakyThrows
+    @Test
+    @AllureId("1458351")
+    @DisplayName("C1458351.GetSignals.Проверка курсора и флага hasNext")
+    @Subfeature("Успешные сценарии")
+    @Description("Получение списка сигналов на стратегии")
+    void C1458351() {
+
+        GetSignalsResponse getSignalsResponse;
+
+        createMasterSignal(0, 3, 1, strategyId, tickerSBER, tradingClearingAccountSber,
+            "4289.37", "10", 11);
+        createMasterSignal(0, 2, 2, strategyId, tickerAAPL, tradingClearingAccountAAPL,
+            "128.37", "3", 12);
+        createMasterSignal(0, 1, 3, strategyId, tickerFB, tradingClearingAccountFB,
+            "500.37", "10", 11);
+        createMasterSignal(0, 0, 4, strategyId, tickerNok, tradingClearingAccountNok,
+            "500.37", "10", 12);
+
+        List<MasterSignal> getAllMasterSignals = masterSignalDao.getAllMasterSignal(strategyId);
+        //Сортируем версию по убыванию
+        getAllMasterSignals.stream()
+            .sorted(Comparator.comparing(MasterSignal::getVersion).reversed())
+            .collect(Collectors.toList());
+
+        //Вызываем метод с курсором 1
+        getSignalsResponse = getSignalsResponseWithLimitAndCursor(strategyId, "1", 1);
+        //Проверяем курсор
+        checkCursoreAndHasNextCursor(getSignalsResponse, "null", false);
+        assertThat("items != []", getSignalsResponse.getItems().toString(), is("[]"));
+
+        //Повторно вызываем метод с курсором 2
+        getSignalsResponse = getSignalsResponseWithLimitAndCursor(strategyId, "2", 1);
+
+        checkCursoreAndHasNextCursor(getSignalsResponse, "1", false);
+        //Получаем items из ответа метода и БД, для инструмента
+        List<Signal> getItemsForSBER = getSignalsResponse.getItems().stream()
+            .filter(res -> res.getExchangePosition().getTicker().equals(tickerSBER))
+            .collect(Collectors.toList());
+
+        Optional<MasterSignal> getDataFromDaoForSBER =  getAllMasterSignals.stream()
+            .filter(signals -> signals.getTicker().equals(tickerSBER))
+            .findFirst();
+
+        //Проверяем items
+        checkItemsFromResponce(getItemsForSBER, getDataFromDaoForSBER, "sell", StrategyCurrency.usd);
+        //Повторно вызываем метод с курсором 3
+        getSignalsResponse = getSignalsResponseWithLimitAndCursor(strategyId, "3", 1);
+
+        //Проверяем курсор и items
+        checkCursoreAndHasNextCursor(getSignalsResponse, "2", true);
+        //Получаем items из ответа метода и БД, для инструмента
+        List<Signal> getItemsForAAPL = getSignalsResponse.getItems().stream()
+            .filter(res -> res.getExchangePosition().getTicker().equals(tickerAAPL))
+            .collect(Collectors.toList());
+
+        Optional<MasterSignal> getDataFromDaoForAAPL =  getAllMasterSignals.stream()
+            .filter(signals -> signals.getTicker().equals(tickerAAPL))
+            .findFirst();
+        //Проверяем items
+        checkItemsFromResponce(getItemsForAAPL, getDataFromDaoForAAPL, "buy", StrategyCurrency.usd);
+    }
+
+    @SneakyThrows
+    @Test
+    @AllureId("1458356")
+    @DisplayName("С1458356.Не нашли сигналы по стратегии")
+    @Subfeature("Успешные сценарии")
+    @Description("Получение списка сигналов на стратегии")
+    void C1458356() {
+        //Вызываем метод
+        GetSignalsResponse getSignalsResponse = getSignalsResponse(strategyId);
+        //Проверяем курсор
+        checkCursoreAndHasNextCursor(getSignalsResponse, "null", false);
+        assertThat("items != []", getSignalsResponse.getItems().toString(), is("[]"));
+    }
+
+    @SneakyThrows
+    @Test
+    @AllureId("1458358")
+    @DisplayName("C1458358. Проверка настроек max-limit & default-limit")
+    @Subfeature("Успешные сценарии")
+    @Description("Получение списка сигналов на стратегии")
+    void C1458358() {
+
+        GetSignalsResponse getSignalsResponse;
+
+        for (int i = 0; i < 110; i++){
+            createMasterSignal(0, 3, i, strategyId, tickerSBER, tradingClearingAccountSber,
+                "4289.37", "10", 11);
+        }
+        getSignalsResponse = getSignalsResponse(strategyId);
+        //Проверяем, что вернули дефотный лимит 30
+        assertThat("getItems().size() > 30", getSignalsResponse.getItems().size(), is(30));
+        //Вызываем метод с limit > 100
+        getSignalsResponse = getSignalsResponseWithLimitAndCursor(strategyId, "110", 110);
+        //Проверяем, что вернули максимальный лимит 100
+        assertThat("getItems().size() > 100", getSignalsResponse.getItems().size(), is(100));
+    }
+
+
+    void createMasterSignal (int minusDays, int minusHours, int version, UUID strategyId, String ticker, String tradingClearingAccount,
+                            String price, String quantity, int action) {
+        LocalDateTime newLocalDateTime = localDateTime.minusDays(minusDays).minusHours(minusHours);
+        Date convertedDatetime = Date.from(newLocalDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        MasterSignal masterSignal = MasterSignal.builder()
+            .strategyId(strategyId)
+            .version(version)
+            .ticker(ticker)
+            .tradingClearingAccount(tradingClearingAccount)
+            .action((byte) action)
+            .state((byte) 1)
+            .price(new BigDecimal(price))
+            .quantity(new BigDecimal(quantity))
+            .createdAt(convertedDatetime)
+            .build();
+        masterSignalDao.insertIntoMasterSignal(masterSignal);
+    }
+
+    GetSignalsResponse getSignalsResponse (UUID strategyId) {
+         GetSignalsResponse getSignalsResponse = signalApi.getSignals()
+            .reqSpec(r -> r.addHeader(xApiKey, key))
+            .strategyIdPath(strategyId)
+            .xAppNameHeader("invest")
+            .xAppVersionHeader("4.5.6")
+            .xPlatformHeader("ios")
+            .xTcsLoginHeader("login")
+            .respSpec(spec -> spec.expectStatusCode(200))
+            .execute(response -> response.as(GetSignalsResponse.class));
+        return getSignalsResponse;
+    }
+
+    GetSignalsResponse getSignalsResponseWithLimitAndCursor (UUID strategyId, String cursor, int limit) {
+        GetSignalsResponse getSignalsResponse = signalApi.getSignals()
+            .reqSpec(r -> r.addHeader(xApiKey, key))
+            .strategyIdPath(strategyId)
+            .xAppNameHeader("invest")
+            .xAppVersionHeader("4.5.6")
+            .xPlatformHeader("ios")
+            .xTcsLoginHeader("login")
+            .cursorQuery(cursor)
+            .limitQuery(limit)
+            .respSpec(spec -> spec.expectStatusCode(200))
+            .execute(response -> response.as(GetSignalsResponse.class));
+        return getSignalsResponse;
+    }
+
+    void checkItemsFromResponce (List<Signal> items, Optional<MasterSignal> masterSignal, String action, StrategyCurrency currency){
+
+        OffsetDateTime offsetDateTime = masterSignal.get().getCreatedAt().toInstant().atOffset(ZoneOffset.ofHours(3));
+        assertThat("createdAt != " + offsetDateTime, items.get(0).getCreatedAt().toInstant().atOffset(ZoneOffset.ofHours(3)),
+         is(offsetDateTime));
+        assertThat("quantity != " + masterSignal.get().getQuantity(), items.get(0).getQuantity(),
+            is(masterSignal.get().getQuantity()));
+        assertThat("action != " + action, items.get(0).getAction().toString(),
+            is(action));
+        assertThat("price.value  != " + masterSignal.get().getPrice(), items.get(0).getPrice().getValue(),
+            is(masterSignal.get().getPrice()));
+        assertThat("price.currency " + currency, items.get(0).getPrice().getCurrency().toString(),
+            is(currency.toString()));
+        assertThat("exchangePosition.ticker != " + masterSignal.get().getTicker(), items.get(0).getExchangePosition().getTicker(),
+            is(masterSignal.get().getTicker()));
+        assertThat("exchangePosition.tradingClearingAccount != " + masterSignal.get().getTradingClearingAccount(), items.get(0).getExchangePosition().getTradingClearingAccount(),
+            is(masterSignal.get().getTradingClearingAccount()));
+        assertThat("version != "+ masterSignal.get().getVersion(), items.get(0).getVersion(),
+            is(masterSignal.get().getVersion()));
+    }
+
+    void checkCursoreAndHasNextCursor (GetSignalsResponse getSignalsResponse, String nextCursor, Boolean hasNext){
+        if(nextCursor == "null"){
+            nextCursor = null;
+        }
+        assertThat("nextCursor != " + nextCursor, getSignalsResponse.getNextCursor(),
+            is(nextCursor));
+        assertThat("hasNext != " + hasNext, getSignalsResponse.getHasNext(), is(hasNext));
+    }
+}
