fa9e6ef1bd20f38383ce81f1fad83748fe195793
Мотин Богдан Юрьевич <b.motin@tinkoff.ru>
2022-01-27 10:40:09 +0000

    Создал новый класс enabledContractSynchronization. Написал тесты к задаче TAP-12231

---
+++ src/test/java/stpTrackingAdminApi/enableContractSynchronization/EnableContractSynchronizationTest.java
@@ -0,0 +1,235 @@
+package stpTrackingAdminApi.enableContractSynchronization;
+
+import extenstions.RestAssuredExtension;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.junit5.AllureJunit5;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.qa.tinkoff.allure.Subfeature;
+import ru.qa.tinkoff.investTracking.configuration.InvestTrackingAutoConfiguration;
+import ru.qa.tinkoff.investTracking.entities.MasterPortfolio;
+import ru.qa.tinkoff.investTracking.entities.SlavePortfolio;
+import ru.qa.tinkoff.investTracking.services.SlavePortfolioDao;
+import ru.qa.tinkoff.kafka.configuration.KafkaAutoConfiguration;
+import ru.qa.tinkoff.kafka.services.ByteArrayReceiverService;
+import ru.qa.tinkoff.social.configuration.SocialDataBaseAutoConfiguration;
+import ru.qa.tinkoff.steps.StpTrackingAdminStepsConfiguration;
+import ru.qa.tinkoff.steps.StpTrackingApiStepsConfiguration;
+import ru.qa.tinkoff.steps.StpTrackingSlaveStepsConfiguration;
+import ru.qa.tinkoff.steps.trackingAdminSteps.StpTrackingAdminSteps;
+import ru.qa.tinkoff.steps.trackingSlaveSteps.StpTrackingSlaveSteps;
+import ru.qa.tinkoff.swagger.investAccountPublic.model.GetBrokerAccountsResponse;
+import ru.qa.tinkoff.swagger.tracking_admin.api.ContractApi;
+import ru.qa.tinkoff.swagger.tracking_admin.invoker.ApiClient;
+import ru.qa.tinkoff.tracking.configuration.TrackingDatabaseAutoConfiguration;
+import ru.qa.tinkoff.tracking.entities.Subscription;
+import ru.qa.tinkoff.tracking.entities.enums.ContractState;
+import ru.qa.tinkoff.tracking.entities.enums.StrategyCurrency;
+import ru.qa.tinkoff.tracking.entities.enums.StrategyStatus;
+import ru.qa.tinkoff.tracking.entities.enums.SubscriptionStatus;
+import ru.qa.tinkoff.tracking.services.database.*;
+import ru.tinkoff.trading.tracking.Tracking;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import static io.qameta.allure.Allure.step;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Durations.TEN_SECONDS;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static ru.qa.tinkoff.kafka.Topics.TRACKING_SLAVE_COMMAND;
+
+@Slf4j
+@Epic("enableContractSynchronization - Включить синхронизацию позиций в slave-портфеле в обе стороны")
+@ExtendWith({AllureJunit5.class, RestAssuredExtension.class})
+@DisplayName("stp-tracking-admin")
+@Tags({@Tag("stp-tracking-admin"), @Tag("enableContractSynchronization")})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(classes = {
+    TrackingDatabaseAutoConfiguration.class,
+    InvestTrackingAutoConfiguration.class,
+    SocialDataBaseAutoConfiguration.class,
+    KafkaAutoConfiguration.class,
+    StpTrackingApiStepsConfiguration.class,
+    StpTrackingSlaveStepsConfiguration.class,
+    StpTrackingAdminStepsConfiguration.class
+})
+
+public class EnableContractSynchronizationTest {
+
+    ContractApi contractApi = ru.qa.tinkoff.swagger.tracking_admin.invoker.ApiClient.api(ApiClient.Config.apiConfig()).contract();
+
+    @Autowired
+    ByteArrayReceiverService kafkaReceiver;
+    @Autowired
+    ClientService clientService;
+    @Autowired
+    ContractService contractService;
+    @Autowired
+    StrategyService strategyService;
+    @Autowired
+    TrackingService trackingService;
+    @Autowired
+    SubscriptionService subscriptionService;
+    @Autowired
+    StpTrackingAdminSteps steps;
+    @Autowired
+    StpTrackingSlaveSteps slaveSteps;
+    @Autowired
+    SlavePortfolioDao slavePortfolioDao;
+
+
+    String siebelIdMaster = "5-23AZ65JU2";
+    String siebelIdSlave = "4-LQB8FKN";
+
+    SlavePortfolio slavePortfolio;
+
+    String contractIdSlave;
+    String contractIdMaster;
+
+    UUID investIdSlave;
+    UUID investIdMaster;
+    UUID strategyId;
+    Subscription subscription;
+
+    String title;
+    String description;
+    String xApiKey = "x-api-key";
+    String key = "tracking";
+
+    String ticker = "AAPL";
+    String tradingClearingAccount = "TKCBM_TCAB";
+
+    @BeforeAll
+    void getDataClients() {
+        //получаем данные по клиенту master в api сервиса счетов
+        GetBrokerAccountsResponse resAccountMaster = steps.getBrokerAccounts(siebelIdMaster);
+        investIdMaster = resAccountMaster.getInvestId();
+        contractIdMaster = resAccountMaster.getBrokerAccounts().get(0).getId();
+        //получаем данные по клиенту slave в api сервиса счетов
+        GetBrokerAccountsResponse resAccountSlave = steps.getBrokerAccounts(siebelIdSlave);
+        investIdSlave = resAccountSlave.getInvestId();
+        contractIdSlave = resAccountSlave.getBrokerAccounts().get(0).getId();
+    }
+
+    @AfterEach
+    void deleteClient() {
+        step("Удаляем клиента автоследования", () -> {
+            try {
+                subscriptionService.deleteSubscription(subscriptionService.getSubscriptionByContract(contractIdSlave));
+            } catch (Exception e) {
+            }
+            try {
+                contractService.deleteContract(contractService.getContract(contractIdSlave));
+            } catch (Exception e) {
+            }
+            try {
+                clientService.deleteClient(clientService.getClient(investIdSlave));
+            } catch (Exception e) {
+            }
+            try {
+                trackingService.deleteStrategy(strategyService.getStrategy(strategyId));
+            } catch (Exception e) {
+            }
+            try {
+                contractService.deleteContract(contractService.getContract(contractIdMaster));
+            } catch (Exception e) {
+            }
+            try {
+                clientService.deleteClient(clientService.getClient(investIdMaster));
+            } catch (Exception e) {
+            }
+        });
+    }
+
+    @BeforeEach
+    void getStrategyData(){
+        title = "Autotest" + randomNumber(0,100);
+        description = "Autotest getOrders";
+        strategyId = UUID.randomUUID();
+    }
+
+    @SneakyThrows
+    @Test
+    @AllureId("1398759")
+    @DisplayName("C1398759.enabledContractSynchronization. Успешная отправка события на синхронизацию")
+    @Subfeature("Успешные сценарии")
+    @Description("Алгоритм предназначен для выставления заявки по выбранной для синхронизации позиции через вызов Middle.")
+    void C1398759() {
+        //создаем в БД tracking данные по ведущему: client, contract, strategy в статусе active
+        steps.createClientWithContractAndStrategyNew(siebelIdMaster, investIdMaster, null, contractIdMaster, null, ContractState.untracked,
+            strategyId, title, description, StrategyCurrency.usd, ru.qa.tinkoff.tracking.entities.enums.StrategyRiskProfile.aggressive,
+            StrategyStatus.active, 0, LocalDateTime.now());
+        // создаем портфель ведущего с позицией в кассандре
+        OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
+        Date date = Date.from(utc.toInstant());
+        List<MasterPortfolio.Position> masterPos = slaveSteps.createListMasterPositionWithOnePos(ticker, tradingClearingAccount,
+            "5", date, 2, slaveSteps.createPosAction(Tracking.Portfolio.Action.SECURITY_BUY_TRADE));
+        steps.createMasterPortfolio(contractIdMaster, strategyId, 4, "6551.10", masterPos);
+        //создаем подписку на стратегию
+        OffsetDateTime startSubTime = OffsetDateTime.now();
+        slaveSteps.createSubcription(investIdSlave, contractIdSlave, null, ContractState.tracked,
+            null, strategyId, SubscriptionStatus.active, new java.sql.Timestamp(startSubTime.toInstant().toEpochMilli()), null, false);
+        subscription = subscriptionService.getSubscriptionByContract(contractIdSlave);
+        // создаем портфель slave с позицией в кассандре
+        String baseMoneySl = "7000.0";
+        List<SlavePortfolio.Position> createListSlaveOnePos = slaveSteps.createListSlavePositionWithOnePosLight(ticker, tradingClearingAccount,
+            "2", date);
+        slaveSteps.createSlavePortfolioWithPosition(contractIdSlave, strategyId, 2, 4,
+            baseMoneySl, date, createListSlaveOnePos);
+        //Вычитываем из топика кафка tracking.event все offset
+        steps.resetOffsetToLate(TRACKING_SLAVE_COMMAND);
+        //вызываем метод enableContractSynchronization
+        contractApi.enableContractSynchronization()
+            .reqSpec(r -> r.addHeader(xApiKey, key))
+            .xAppNameHeader("invest")
+            .xTcsLoginHeader("tracking")
+            .contractIdPath(contractIdSlave)
+            .respSpec(spec -> spec.expectStatusCode(202))
+            .execute(response -> response);
+        //Смотрим, сообщение, которое поймали в топике kafka
+        List<Pair<String, byte[]>> messages = kafkaReceiver.receiveBatch(TRACKING_SLAVE_COMMAND, Duration.ofSeconds(20));
+        Pair<String, byte[]> message = messages.stream()
+            .sorted(Collections.reverseOrder())
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Сообщений не получено"));
+        Tracking.PortfolioCommand portfolioCommand = Tracking.PortfolioCommand.parseFrom(message.getValue());
+        //получаем портфель slave
+        await().atMost(TEN_SECONDS).until(() ->
+            slavePortfolio = slavePortfolioDao.getLatestSlavePortfolio(contractIdSlave, strategyId), notNullValue());
+        //Проверяем, данные в сообщении
+        checkEventParams(portfolioCommand, contractIdSlave, "ENABLE_SYNCHRONIZATION");
+    }
+
+
+
+
+    //метод рандомайза для номера теста
+    public static int randomNumber(int min, int max) {
+        int number = min + (int) (Math.random() * max);
+        return number;
+    }
+
+    //Проверяем параметры события
+    void checkEventParams(Tracking.PortfolioCommand portfolioCommand, String contractId, String operation) {
+        assertThat("ID договора не равен", portfolioCommand.getContractId(), is(contractId));
+        assertThat("Operation не равен", portfolioCommand.getOperation(), is(Tracking.PortfolioCommand.Operation.ENABLE_SYNCHRONIZATION));
+
+    }
+
+}
