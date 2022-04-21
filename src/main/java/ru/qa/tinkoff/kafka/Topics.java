bbe9974cefb00d0032803c2ce727b6af34456dee
Yevgeniy Belyaninov <y.belyaninov@andersenlab.com>
2022-04-21 15:51:21 +0300

    Добавил новую биржу

--- src/main/java/ru/qa/tinkoff/kafka/Topics.java
+++ src/main/java/ru/qa/tinkoff/kafka/Topics.java
@@ -30,6 +30,7 @@ public enum Topics {
     TRACKING_FX_RETRYER_COMMAND("tracking.fx.retryer.command"),
     TRACKING_FX_WEEKEND_RETRYER_COMMAND("tracking.fx-weekend.retryer.command"),
     TRACKING_FX_MTL_RETRYER_COMMAND("tracking.fx-mtl.retryer.command"),
+    TRACKING_MOEX_WEEKEND_RETRYER_COMMAND("tracking.moex-weekend.retryer.command"),
     TRACKING_FEE_COMMAND("tracking.fee.command"),
     MIOF_POSITIONS_RAW("miof.positions.raw"),
     TRACKING_CONTRACT_EVENT("tracking.contract.event"),
